259c84484ad32cf7c4d6d161f2dbfa77f25ccb33
DevynSlaughter <82952900+DevynSlaughter@users.noreply.github.com>
2021-05-17 08:49:46 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "(m_~m7+mpjzM@Ef3)kRB":
+            case "DABABY":return img`
+. . . 8 8 8 8 8 1 8 8 . . . . . 
+. . . 8 8 1 8 1 8 1 8 . . . . . 
+. . . 8 8 1 1 8 1 8 1 . . . . . 
+. . . 8 8 1 8 8 8 8 8 . . . . . 
+. . 8 8 8 1 1 8 8 8 8 8 . . . . 
+. . . . e e e e e e e . . . . . 
+. . . e e e e e e e e e . . . . 
+. . . e f f e e f f e e . . . . 
+. e e e e e e e e e e e e e . . 
+. e e e e e e e e e e e e e . . 
+. . e e 1 1 1 1 1 1 e e e . . . 
+. . . e e 1 1 1 1 e e e . . . . 
+. . . e e e e e e e e e . . . . 
+. . . . e e e e e e e . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "{7jjUG#S`GENz^:":
+            case "PolarBear":return img`
+. . . . 1 . . . 1 . . . . . . . 
+. . . . 1 1 1 1 1 . . . . . . . 
+. . . . 1 f 1 f 1 1 1 . . . . . 
+. . . . 1 1 1 1 1 1 f . . . . . 
+. . . 1 1 1 3 1 1 1 1 . . . . . 
+. . . 1 1 1 1 3 3 3 . . . . . . 
+. . . 1 1 1 1 1 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 . . . . . . 
+. 1 1 1 1 1 1 1 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 . . . . . . 
+. . . . 1 1 1 1 1 . . . . . . . 
+. . . . 1 1 . 1 1 . . . . . . . 
+. . . . 1 1 . 1 1 . . . . . . . 
+. . . . 1 1 . 1 1 . . . . . . . 
+. . . . f f . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000202010000000101000300000000000102020200050102000001010000000002020202040202020101020201010101020102020102020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . 2 2 . . 
+. . . . . . . . . . . . 2 2 2 . 
+. . 2 2 . 2 . . . . . 2 2 2 2 . 
+2 2 2 . . 2 2 . . . . 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile2,myTiles.tile3,myTiles.tile1,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "ice":
+            case "tile2":return tile2;
+            case "bottomIce":
+            case "tile3":return tile3;
+            case "spawnPoint":
+            case "tile1":return tile1;
+            case "myTile":
+            case "tile4":return tile4;
+            case "myTile0":
+            case "tile5":return tile5;
+            case "myTile1":
+            case "tile6":return tile6;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
